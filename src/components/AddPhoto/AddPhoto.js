1ce5d3d903315993bca151572452eccfbad6e989
Austin Michael Regan <austinmregan@gmail.com>
2019-08-21 14:24:47 -0400

    can connect to backend

--- src/components/AddPhoto/AddPhoto.js
+++ src/components/AddPhoto/AddPhoto.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./AddPhoto.css";
 import { createNewPhoto } from "../../actions/photosAndCollections";
+import axios from "axios";
 
 export default class AddPhoto extends Component {
   constructor(props) {
@@ -20,54 +21,39 @@ export default class AddPhoto extends Component {
     evt.preventDefault();
     this.setState({ [evt.target.name]: evt.target.value });
   }
-  handleSubmit(evt) {
-    evt.preventDefault();
-    let url = evt.target[0].value;
-    // let birthplace = evt.target[1].value;
-    // let team = evt.target[2].value;
-    // let rank = evt.target[3].value;
-    // let image = evt.target[4].value;
-
-    this.props.dispatch(createNewPhoto(url));
-
-    evt.target[0].value = "";
-    // evt.target[1].value = "";
-    // evt.target[2].value = "";
-    // evt.target[3].value = "";
-    // evt.target[4].value = "";
+  handleSubmit() {
+    axios
+      .post("https://pinstagram500-api.herokuapp.com/", this.state)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => {
+        console.error(err);
+      });
   }
   render() {
     return (
-      <form className="form" onSubmit={this.handleSubmit}>
-        <div classname="centerFlex">
-          <div className="formGrid">
-            <div className="driverTitle">Add a Photo</div>
-            <div className="name">
-              <label>Image:</label>
-              <input className="inputField" type="text" />
-            </div>
-            {/* <div className="birthplace">
-              <label>Birthplace:</label>
-              <input className="inputField" type="text" />
-            </div>
-            <div className="team">
-              <label>Team:</label>
-              <input className="inputField" type="text" />
-            </div>
-            <div className="rank">
-              <label>Rank:</label>
-              <input className="inputField" type="number" />
-            </div>
-            <div className="image">
-              <label>Image:</label>
-              <input className="inputField" type="text" />
-            </div> */}
-            <button className="submit" type="submit">
-              Submit
-            </button>
+      <div className="form">
+        <div className="driverGrid2">
+          <div className="driverTitle">Add a Photo</div>
+          <div className="name">
+            <p>Image</p>
+            <input
+              className="extend"
+              type="text"
+              name="url"
+              name={this.state.url}
+              onChange={this.handleChange}
+            />
           </div>
+          <input
+            className="submit"
+            type="submit"
+            value="Submit"
+            onClick={this.handleSubmit}
+          />
         </div>
-      </form>
+      </div>
     );
   }
 }
