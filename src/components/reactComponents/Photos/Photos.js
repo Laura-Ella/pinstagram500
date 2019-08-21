10773b12212129a38856eb06f3f9706b6670b277
Austin Michael Regan <austinmregan@gmail.com>
2019-08-21 18:41:43 -0400

    delete request works on backup

--- src/components/reactComponents/Photos/Photos.js
+++ src/components/reactComponents/Photos/Photos.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./Photos.css";
+import axios from "axios";
 
 class Photos extends Component {
+  onClick = () => {
+    axios
+      .delete(
+        `${"https://pinstagram500-api.herokuapp.com/5d5dc756d8e8cf001721d787"}`
+      )
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
   render() {
     let photo = this.props.photoData.map((photo, index) => {
       console.log(photo.url);
@@ -39,13 +52,16 @@ class Photos extends Component {
               </p>
             </div>
           </div>
+          <button className="delete" onClick={this.onClick}>
+            X
+          </button>
         </div>
       );
     });
     return (
       <div>
         <div>
-          <div className="edits">
+          {/* <div className="edits">
             <Link to="/drivers/remove">
               <div className="editDivs3">
                 <p>Remove</p>
@@ -61,7 +77,7 @@ class Photos extends Component {
                 <p>Add</p>
               </div>
             </Link>
-          </div>
+          </div> */}
           <div className="driverGrid">{photo}</div>
         </div>
       </div>
