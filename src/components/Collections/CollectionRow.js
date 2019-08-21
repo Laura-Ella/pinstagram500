0ba5f6a4c03a51033d19c96640ceb8f4f51f9ca8
Austin Michael Regan <austinmregan@gmail.com>
2019-08-21 11:14:05 -0400

    can see multiple photos

--- src/components/Collections/CollectionRow.js
+++ src/components/Collections/CollectionRow.js
@@ -17,16 +17,32 @@ const CollectionRow = ({
       <div
         className="photo"
         style={{
-          backgroundImage: `url(${urls[0]})`,
-          backgroundPosition: "center",
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          position: "relative",
-          textAlign: "center",
-          border: "2px solid black"
+          backgroundImage: `url(${urls[0]})`
         }}
-      >
-        {/* <div className="textContainer">
+      />
+      <div
+        className="photo"
+        style={{
+          backgroundImage: `url(${urls[1]})`
+        }}
+      />
+      <div
+        className="photo"
+        style={{
+          backgroundImage: `url(${urls[2]})`
+        }}
+      />
+      <button className="delete" onClick={onClick}>
+        X
+      </button>
+    </div>
+  </div>
+);
+
+export default CollectionRow;
+
+{
+  /* <div className="textContainer">
           <div className="text">
             <p>
               <span className="description">Name: </span>
@@ -45,25 +61,5 @@ const CollectionRow = ({
               {rank}
             </p>
           </div>
-        </div> */}
-      </div>
-      <div
-        className="photo"
-        style={{
-          backgroundImage: `url(${urls[1]})`,
-          backgroundPosition: "center",
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          position: "relative",
-          textAlign: "center",
-          border: "2px solid black"
-        }}
-      />
-      <button className="delete" onClick={onClick}>
-        X
-      </button>
-    </div>
-  </div>
-);
-
-export default CollectionRow;
+        </div> */
+}
